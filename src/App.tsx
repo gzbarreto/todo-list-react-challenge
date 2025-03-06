90bb7d4f283bea56326ec3958e4ecd1741f13e62
gabs <gabrielazbarreto@gmail.com>
2025-03-06 17:56:41 -0300

    start app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import styles from "./App.module.css"
 
 function App() {
-  return <h1>hello world</h1>
+  return (
+    <div className={styles.app}>
+      <header>
+        <img src="src\assets\logo.png" />
+      </header>
+
+      <form>
+        <input placeholder="Adicione uma nova tarefa..."></input>
+        <button>
+          Criar
+          {/* TODO: ADD ICON */}
+          </button>
+      </form>
+
+      <div>task list</div>
+    </div>
+  )
 }
 
 export default App
