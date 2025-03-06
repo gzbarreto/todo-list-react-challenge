4182516825a544751608307ae1f792bb0dc89cf9
gabs <gabrielazbarreto@gmail.com>
2025-03-06 19:54:18 -0300

    add task list header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import styles from "./App.module.css"
 
 import { PlusCircle } from "@phosphor-icons/react"
 
-
 function App() {
   return (
     <div className={styles.app}>
@@ -12,11 +11,20 @@ function App() {
 
       <form className={styles.newTask}>
         <input placeholder="Adicione uma nova tarefa..."></input>
-        <button className={styles.buttonCreate}>
+        <button type="submit" className={styles.buttonCreate}>
           Criar
           <PlusCircle size={20} />
-          </button>
+        </button>
       </form>
+
+      <div className={styles.taskHeader}>
+        <div className={styles.createdTasks}>
+          Tarefas criadas  <span>5</span>
+        </div>
+        <div className={styles.doneTasks}>
+          Concluídas  <span>2 de 5</span>
+        </div>
+      </div>
 
       <div>task list</div>
     </div>
